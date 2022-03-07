1a51fe4ce0a9e09687c717724a5358329d84c259
SeetZy <64326465+SeetZy@users.noreply.github.com>
2022-03-07 21:18:58 +0200

    moved everything to pyqt

--- main.py
+++ main.py
@@ -1,53 +1,98 @@
-import tkinter
-from tkinter import *
-from tkinter import messagebox
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMessageBox
 from random import randint
 
-root = Tk()
-root.title('Password Gen')
-# root.iconbitmap()
-root.geometry("800x400")
 
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(490, 506)
+        font = QtGui.QFont()
+        font.setFamily("Tahoma")
+        MainWindow.setFont(font)
+        MainWindow.setFixedSize(MainWindow.size())
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
 
-def rand_pass():
-    entry2.delete(0, END)
-    pass_len = int(entry.get())
-    generated_pass = ''
+        self.btngen = QtWidgets.QPushButton(self.centralwidget)
+        self.btngen.setGeometry(QtCore.QRect(60, 350, 161, 61))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.btngen.setFont(font)
+        self.btngen.setObjectName("btngen")
+        self.btngen.clicked.connect(self.rand_pass)
 
-    for i in range(pass_len):
-        generated_pass += chr(randint(33, 126))
+        self.btncopy = QtWidgets.QPushButton(self.centralwidget)
+        self.btncopy.setGeometry(QtCore.QRect(250, 350, 161, 61))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.btncopy.setFont(font)
+        self.btncopy.setObjectName("btncopy")
+        self.btncopy.clicked.connect(self.copy_pass)
 
-    entry2.insert(0, generated_pass)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(90, 50, 311, 41))
+        font = QtGui.QFont()
+        font.setFamily("Tahoma")
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
 
-    if entry2 == '':
-        messagebox.showerror("Uh oh...", "You need to enter a length for the password")
-    else:
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(90, 110, 281, 31))
+        font = QtGui.QFont()
+        font.setFamily("Tahoma")
+        font.setPointSize(12)
+        self.textEdit.setFont(font)
+        self.textEdit.setObjectName("textEdit")
+
+        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit_2.setEnabled(True)
+        self.textEdit_2.setGeometry(QtCore.QRect(90, 220, 281, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.textEdit_2.setFont(font)
+        self.textEdit_2.setObjectName("textEdit_2")
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 490, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Password Generator"))
+        self.btngen.setText(_translate("MainWindow", "Generate Password"))
+        self.btncopy.setText(_translate("MainWindow", "Copy To Clipboard"))
+        self.label.setText(_translate("MainWindow", "Enter the length of the password"))
+
+    def rand_pass(self):
+        self.textEdit_2.setText("")
+
+        generated_pass = ''
+
+        pass_len = int(self.textEdit.toPlainText())
+
+        for i in range(pass_len):
+            generated_pass += chr(randint(33, 126))
+
+        self.textEdit_2.setText(f'{generated_pass}')
+
+    def copy_pass(self):
         pass
 
-
-def copy_pass():
-    root.clipboard_clear()
-    root.clipboard_append(entry2.get())
-
-    messagebox.showinfo("Copied!", "The password you generated\nhas been saved to your clipboard")
-
-
-frame = LabelFrame(root, text="Choose the length of the password")
-frame.pack(padx=40, pady=40, side=RIGHT)
-
-entry = Entry(frame, font=("Roboto", 24))
-entry.pack(pady=20, padx=20)
-
-entry2 = Entry(frame, text='', font=("Roboto", 24))
-entry2.pack(padx=20, pady=20, side=RIGHT)
-
-btn_frame = Frame(root)
-btn_frame.pack(padx=20, side=tkinter.LEFT)
-
-btn_gen = Button(btn_frame, text="Generate password", command=rand_pass)
-btn_gen.grid(row=0, column=0, pady=40)
-
-btn_copy = Button(btn_frame, text="Copy To Clipboard", command=copy_pass)
-btn_copy.grid(row=1, column=0)
-
-root.mainloop()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
