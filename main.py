45f200df43d5b5375b86b5db7ba37387ff431a1a
SeetZy <64326465+SeetZy@users.noreply.github.com>
2022-03-09 14:04:55 +0200

    final design and everything works needs final code cleanup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtGui import QCursor
 from random import randint
 import sys
 
@@ -7,32 +8,41 @@ import sys
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(490, 506)
+        MainWindow.resize(471, 652)
         font = QtGui.QFont()
         font.setFamily("Tahoma")
         MainWindow.setFont(font)
         MainWindow.setFixedSize(MainWindow.size())
+        MainWindow.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(40, 80, 391, 450))
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+
         self.btngen = QtWidgets.QPushButton(self.centralwidget)
-        self.btngen.setGeometry(QtCore.QRect(60, 350, 161, 61))
+        self.btngen.setGeometry(QtCore.QRect(60, 400, 161, 61))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.btngen.setFont(font)
         self.btngen.setObjectName("btngen")
         self.btngen.clicked.connect(self.rand_pass)
+        self.btngen.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
 
         self.btncopy = QtWidgets.QPushButton(self.centralwidget)
-        self.btncopy.setGeometry(QtCore.QRect(250, 350, 161, 61))
+        self.btncopy.setGeometry(QtCore.QRect(250, 400, 161, 61))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.btncopy.setFont(font)
         self.btncopy.setObjectName("btncopy")
         self.btncopy.clicked.connect(self.copy_pass)
+        self.btncopy.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
 
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(90, 50, 311, 41))
+        self.label.setGeometry(QtCore.QRect(90, 120, 311, 41))
         font = QtGui.QFont()
         font.setFamily("Tahoma")
         font.setPointSize(12)
@@ -40,7 +50,7 @@ class Ui_MainWindow(object):
         self.label.setObjectName("label")
 
         self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(90, 110, 281, 31))
+        self.textEdit.setGeometry(QtCore.QRect(90, 160, 291, 31))
         font = QtGui.QFont()
         font.setFamily("Tahoma")
         font.setPointSize(12)
@@ -49,15 +59,23 @@ class Ui_MainWindow(object):
 
         self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
         self.textEdit_2.setEnabled(True)
-        self.textEdit_2.setGeometry(QtCore.QRect(90, 220, 281, 31))
+        self.textEdit_2.setGeometry(QtCore.QRect(90, 290, 291, 71))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.textEdit_2.setFont(font)
         self.textEdit_2.setObjectName("textEdit_2")
 
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(90, 250, 311, 41))
+        font = QtGui.QFont()
+        font.setFamily("Tahoma")
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 490, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 471, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -73,20 +91,25 @@ class Ui_MainWindow(object):
         self.btngen.setText(_translate("MainWindow", "Generate Password"))
         self.btncopy.setText(_translate("MainWindow", "Copy To Clipboard"))
         self.label.setText(_translate("MainWindow", "Enter the length of the password"))
+        self.label_2.setText(_translate("MainWindow", "Generated Password"))
 
     def rand_pass(self):
         self.textEdit_2.setText("")
-        generated_pass = ''
+        self.generated_pass = ''
 
         pass_len = int(self.textEdit.toPlainText())
 
         for i in range(pass_len):
-            generated_pass += chr(randint(33, 126))
-
-        self.textEdit_2.setText(f'{generated_pass}')
+            self.generated_pass += chr(randint(33, 126))
+            self.textEdit_2.setText(f'{self.generated_pass}')
+            self.btncopy.setText("Copy To Clipboard")
 
     def copy_pass(self):
-        pass
+        cb = QApplication.clipboard()
+        cb.clear(mode=cb.Clipboard)
+        cb.setText(self.generated_pass, mode=cb.Clipboard)
+
+        self.btncopy.setText("Copied!")
 
 
 if __name__ == "__main__":
